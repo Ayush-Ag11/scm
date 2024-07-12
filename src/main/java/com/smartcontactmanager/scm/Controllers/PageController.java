24f07de007cf868ff016ac1bd5012e758cbc575b
Ayush-Ag11 <ayushagnihotri22@gmail.com>
2024-07-12 16:56:51 +0530

    Setting up thymeleaf fragments

--- src/main/java/com/smartcontactmanager/scm/Controllers/PageController.java
+++ src/main/java/com/smartcontactmanager/scm/Controllers/PageController.java
@@ -2,17 +2,35 @@ package com.smartcontactmanager.scm.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
-    @RequestMapping("/home")
-    public String home(Model model){
-        System.out.println("Home page handler");
-        model.addAttribute("name","Ayush");
-        model.addAttribute("gitRepository","https://github.com/Ayush-Ag11/scm.git");
-        return "home";
+    private static final String HOME_VIEW = "home";
+    private static final String ABOUT_VIEW = "about";
+    private static final String SERVICES_VIEW = "services";
+
+    private static final String NAME_ATTRIBUTE = "name";
+    private static final String GIT_REPOSITORY_ATTRIBUTE = "gitRepository";
+
+    private static final String DEFAULT_NAME = "Ayush";
+    private static final String DEFAULT_GIT_REPOSITORY = "https://github.com/Ayush-Ag11/scm.git";
+
+    @GetMapping("/home")
+    public String home(Model viewModel) {
+        viewModel.addAttribute(NAME_ATTRIBUTE, DEFAULT_NAME);
+        viewModel.addAttribute(GIT_REPOSITORY_ATTRIBUTE, DEFAULT_GIT_REPOSITORY);
+        return HOME_VIEW;
     }
 
+    @GetMapping("/about")
+    public String about() {
+        return ABOUT_VIEW;
+    }
+
+    @GetMapping("/services")
+    public String services() {
+        return SERVICES_VIEW;
+    }
 }
